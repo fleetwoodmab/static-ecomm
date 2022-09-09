9c20d99863a31215b06c310ed94e033f2adce010
mab <73135677+hacoeur@users.noreply.github.com>
2022-09-09 19:42:08 +0200

    thumbnail hover effects

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 window.onload = function() {
     var mainImg = document.getElementById('Main');
 
-    document.getElementById('TNOne').onclick = function() {
+    one.classList.remove("tn-active");
+    two.classList.remove("tn-active");
+    three.classList.remove("tn-active");
+    four.classList.remove("tn-active");
+
+    document.getElementById('one').onclick = function() {
             mainImg.src = './images/image-product-1.jpg';
-            //alert('one clicked');
+            one.classList.add("tn-active");
+            two.classList.remove("tn-active");
+            three.classList.remove("tn-active");
+            four.classList.remove("tn-active");
     };
-    document.getElementById('TNTwo').onclick = function() {
+    document.getElementById('two').onclick = function() {
             mainImg.src = './images/image-product-2.jpg';
-            //alert('two clicked');
+            one.classList.remove("tn-active");
+            two.classList.add("tn-active");
+            three.classList.remove("tn-active");
+            four.classList.remove("tn-active");
+
+            
     };
 
-    document.getElementById('TNThree').onclick = function() {
+    document.getElementById('three').onclick = function() {
         mainImg.src = './images/image-product-3.jpg';
-        //alert('one clicked');
+        one.classList.remove("tn-active");
+        two.classList.remove("tn-active");
+        three.classList.add("tn-active");
+        four.classList.remove("tn-active");
+
     };
 
-    document.getElementById('TNFour').onclick = function() {
+    document.getElementById('four').onclick = function() {
         mainImg.src = './images/image-product-4.jpg';
-        //alert('two clicked');
+        one.classList.remove("tn-active");
+        two.classList.remove("tn-active");
+        three.classList.remove("tn-active");
+        four.classList.add("tn-active");
+
     };
 };
